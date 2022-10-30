853c8d73e7cb5986ac2ff69e528644c73a8654e5
thuanvo <thuanvip322@gmail.com>
2022-10-30 19:12:02 +0700

    Change message response

--- src/main/java/com/cosmetics/cosmetics/Service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/cosmetics/cosmetics/Service/Impl/CategoryServiceImpl.java
@@ -27,41 +27,41 @@ public class CategoryServiceImpl implements CategoryService{
 	public ResponseEntity<?> createCategory(String CategoryName) {
 		// TODO Auto-generated method stub
 		if(categoryRepository.findByName(CategoryName.toLowerCase()).isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("danh mục không tồn tại",404,CategoryName));
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("Danh mục không tồn tại",404,CategoryName));
 		}
 		Category newCategory = new Category();
 		newCategory.setName(CategoryName);
-		return ResponseEntity.ok(new ResponseModel("tạo thành công",200,categoryRepository.save(newCategory)));
+		return ResponseEntity.ok(new ResponseModel("Tạo thành công",200,categoryRepository.save(newCategory)));
 	}
 
 	@Override
 	public ResponseEntity<?> updateCategory(int id, String CategoryName) {
 		// TODO Auto-generated method stub
 		if(!categoryRepository.findById(id).isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("danh mục không tồn tại",404,CategoryName));
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("Danh mục không tồn tại",404,CategoryName));
 		}
 		Category newCategory = categoryRepository.findById(id).get();
 		newCategory.setName(CategoryName);
-		return ResponseEntity.ok(new ResponseModel("tạo thành công",200,categoryRepository.save(newCategory)));
+		return ResponseEntity.ok(new ResponseModel("Tạo thành công",200,categoryRepository.save(newCategory)));
 	}
 
 	@Override
 	public ResponseEntity<?> deleteCategory(int id) {
 		// TODO Auto-generated method stub
 		if(!categoryRepository.findById(id).isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("danh mục không tồn tại",404,id));
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseModel("Danh mục không tồn tại",404,id));
 		}
 		if(typeRepository.findByCategory(categoryRepository.findById(id).get()).size() != 0) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseModel("Không thể xóa danh mục chứa loại sản phẩm",409,id));
 		}
 		categoryRepository.deleteById(id);
-		return ResponseEntity.ok(new ResponseModel("xóa thành công",200,id));
+		return ResponseEntity.ok(new ResponseModel("Xóa thành công",200,id));
 	}
 
 	@Override
 	public ResponseEntity<?> getAll() {
 		// TODO Auto-generated method stub
-		return ResponseEntity.ok(new ResponseModel("thành công",200,categoryRepository.findAll()));
+		return ResponseEntity.ok(new ResponseModel("Thành công",200,categoryRepository.findAll()));
 	}
 
 }
